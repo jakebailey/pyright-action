b6ed11164a59cf2d42e2c582ff77e8f75545ff06
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-05-03 11:37:50 -0700

    Use v8 coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     collectCoverage: true,
+    coverageProvider: 'v8',
     collectCoverageFrom: ['src/**/*.ts'],
     coveragePathIgnorePatterns: ['src/index.ts'], // Just the bundle entrypoint
 };
